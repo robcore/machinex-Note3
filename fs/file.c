27c54991017a0f29dbb1cac5ddea4960be867b15
robcore <robpatershuk@gmail.com>
2016-11-12 22:02:08 -0700

    oops, i zigged when I should have zagged

--- fs/file.c
+++ fs/file.c
@@ -264,6 +264,13 @@ int expand_files(struct files_struct *files, int nr)
 
 	fdt = files_fdtable(files);
 
+	/*
+	 * N.B. For clone tasks sharing a files structure, this test
+	 * will limit the total number of files that can be opened.
+	 */
+	if (nr >= rlimit(RLIMIT_NOFILE))
+		return -EMFILE;
+
 	/* Do we need to expand? */
 	if (nr < fdt->max_fds)
 		return 0;
@@ -545,6 +552,7 @@ int alloc_fd(unsigned start, unsigned flags)
 {
 	struct files_struct *files = current->files;
 	unsigned int fd;
+	unsigned end = rlimit(RLIMIT_NOFILE);
 	int error;
 	struct fdtable *fdt;
 
