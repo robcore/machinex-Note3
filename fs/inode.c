edb9755985c4b9f65e733ca4fe93df46f307a4b0
robcore <robpatershuk@gmail.com>
2016-11-18 05:18:04 -0700

    mm-add-a-low-limit-to-alloc_large_system_hash

--- fs/inode.c
+++ fs/inode.c
@@ -1659,6 +1659,7 @@ void __init inode_init_early(void)
 					HASH_EARLY,
 					&i_hash_shift,
 					&i_hash_mask,
+					0,
 					0);
 
 	for (loop = 0; loop < (1U << i_hash_shift); loop++)
@@ -1689,6 +1690,7 @@ void __init inode_init(void)
 					0,
 					&i_hash_shift,
 					&i_hash_mask,
+					0,
 					0);
 
 	for (loop = 0; loop < (1U << i_hash_shift); loop++)
